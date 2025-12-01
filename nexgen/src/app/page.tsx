1ba0aac7810ea94ad1b1e83421b535f0ef2d9ae6
Luka Gogichaishvili <luka595664@gmail.com>
2025-12-01 21:08:49 +0400

    feat: reasons section

--- nexgen/src/app/page.tsx
+++ nexgen/src/app/page.tsx
@@ -1,4 +1,5 @@
 import { headerButtons } from "@/common/data/header-buttons";
+import { reasons } from "@/common/data/reasons";
 
 export default function Home() {
   return (
@@ -100,7 +101,32 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section id="reasons"></section>
+        <section id="reasons">
+          <div className="reasons-heading-container">
+            <h2>REASONS TO CHOOSE NEXGEN FOR YOUR DIGITAL JOURNEY</h2>
+          </div>
+          <div className="reasons-container">
+            {reasons.map((reason) => {
+              return (
+                <div className="reason">
+                  <div className="reason-text">
+                    <h3>{reason.heading}</h3>
+                    <p>{reason.text}</p>
+                  </div>
+                  <div className="learn-more-container">
+                    <div className="open-button-container">
+                      <img
+                        alt="Open button."
+                        src="./images/top-right-arrow.png"
+                      />
+                    </div>
+                    <p>Learn More</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </section>
         <section id="services"></section>
         <section id="projects"></section>
         <section id="testimonials"></section>
---
+++ nexgen/src/common/data/reasons.ts
@@ -0,0 +1,18 @@
+export const reasons = [
+  {
+    heading: "Expertise in Cutting-Edge Technologies",
+    text: "NexGen ensures your projects are powered by state-of-the-art technologies, guaranteeing innovation and future-proof solutions.",
+  },
+  {
+    heading: "Proven Track Record of Success",
+    text: "NexGen demonstrates a consistent ability to meet and exceed client expectations, providing reliable and effective web solutions tailored to diverse needs.",
+  },
+  {
+    heading: "Client-Centric Approach",
+    text: "At NexGen, we prioritize understanding our clients' unique requirements, fostering transparent communication throughout the development process.",
+  },
+  {
+    heading: "Dedicated Team of Professionals",
+    text: "Our professionals bring a wealth of expertise to the table, ensuring the delivery of top-notch, scalable, and secure web solutions for your business.",
+  },
+];
