c853101e4dd4225e0e1f6e81add84d39a4b15f31
Luka Gogichaishvili <luka595664@gmail.com>
2025-12-01 20:21:47 +0400

    feat: header structure
    Will probably have to isolate this element later

--- nexgen/src/app/page.tsx
+++ nexgen/src/app/page.tsx
@@ -1,10 +1,27 @@
+import { headerButtons } from "@/common/data/header-buttons";
+
 export default function Home() {
   return (
     <div>
-      <main>
-        <p>DIGITAL SOLUTIONS</p>
-        <p className="roboto-mono">WEBSITE DESIGN</p>
-      </main>
+      <header>
+        <div className="header-logo-container">
+          <img alt="NexGen Logo." src="./images/image.png" />
+        </div>
+        <div className="header-buttons-container">
+          {headerButtons.map((text) => {
+            return (
+              <div
+                key={text}
+                id={`#${text}`}
+                className="header-button roboto-mono"
+              >
+                {text.toUpperCase()}
+              </div>
+            );
+          })}
+        </div>
+      </header>
+      <main></main>
     </div>
   );
 }
