a83db6a545a555f087401fff39fbfc887f0e8d07
Luka Gogichaishvili <luka595664@gmail.com>
2025-12-02 12:16:15 +0400

    feat: contact section
    feat: contact section digital presence
    
    feat: contact section social cards
    
    feat: contact section nav and newsletter
    
    feat: contact section copyright

---
+++ nexgen/src/common/data/socials.ts
@@ -0,0 +1,22 @@
+export const socials = [
+  {
+    image: "",
+    title: "Instagram",
+    body: "Share visually appealing snippets of our latest web projects.",
+  },
+  {
+    image: "",
+    title: "Twitter",
+    body: "Tweet about interesting coding challenges you've overcome.",
+  },
+  {
+    image: "",
+    title: "Dribbble",
+    body: "Showcase design elements of our web projects.",
+  },
+  {
+    image: "",
+    title: "Behance",
+    body: "Create detailed presentations for our projects.",
+  },
+];
