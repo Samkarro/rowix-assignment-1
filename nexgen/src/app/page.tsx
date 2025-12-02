d0c8db9698d11fddca59d712624a20cebf442152
Luka Gogichaishvili <luka595664@gmail.com>
2025-12-02 11:29:00 +0400

    feat: testimonials section

--- nexgen/src/app/page.tsx
+++ nexgen/src/app/page.tsx
@@ -2,6 +2,7 @@ import { headerButtons } from "@/common/data/header-buttons";
 import { projects } from "@/common/data/projects";
 import { reasons } from "@/common/data/reasons";
 import { services } from "@/common/data/services";
+import { testimonials } from "@/common/data/testimonials";
 
 export default function Home() {
   return (
@@ -249,7 +250,52 @@ export default function Home() {
             );
           })}
         </section>
-        <section id="testimonials"></section>
+        <section id="testimonials">
+          <div className="testimonials-heading-container">
+            <h2>TESTIMONIALS</h2>
+            <div className="upper-service-right">
+              <div className="open-button-container">
+                <img alt="Open button." src="./images/top-right-arrow.png" />
+              </div>
+              <p
+                className="achievement-heading roboto-mono"
+                style={{ fontSize: "20px" }}
+              >
+                ALL TESTIMONIALS
+              </p>
+            </div>
+          </div>
+          <div className="testimonials-container">
+            {testimonials.map((testimonial) => {
+              return (
+                <div className="testimonial">
+                  <div className="testimonial-top">
+                    <h4>{testimonial?.title}</h4>
+                    <p>{testimonial?.body}</p>
+                  </div>
+                  <div className="testimonial-bottom">
+                    <div>
+                      <div className="testimonial-image-container">
+                        <img src={testimonial?.author.image} />
+                      </div>
+                      <div className="testimonial-author-info">
+                        <p className="testimonial-author-name">
+                          {testimonial?.author.name}
+                        </p>
+                        <p className="testimonial-author-position">
+                          {testimonial?.author.position}
+                        </p>
+                      </div>
+                    </div>
+                    <div className="open-button-container">
+                      <img alt="Open button." src="./images/right-arrow.png" />
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </section>
         <section id="faq"></section>
         <section id="contact"></section>
       </main>
