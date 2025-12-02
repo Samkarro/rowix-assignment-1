f3b1282210d580b84dfcd0301a16ac1a33cce724
Luka Gogichaishvili <luka595664@gmail.com>
2025-12-02 11:07:48 +0400

    feat: projects section

---
+++ nexgen/src/common/data/projects.ts
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    title: "ZENITH FITNESS APP",
+    category: "Mobile App Development",
+    timeTaken: "6 months",
+    description:
+      "An all-in-one health and wellness app that offers personalized fitness plans, nutrition guidance, and virtual workout classes.",
+    tech: ["React Native", "Firebase", "Redux", "REST API", "MongoDB"],
+    // TODO: ADD MEMBER ICONS
+    team: [],
+    // TODO: ADD IMAGES
+    image: "",
+  },
+  {
+    title: "A-AURA ECOMMERCE",
+    category: "Web Design & Development",
+    timeTaken: "3 months",
+    description:
+      "A complete overhaul of a corporate website to enhance its brand identity and user experience.",
+    tech: ["Wordpress", "PHP", "HTML5", "CS3", "JavaScript"],
+    // TODO: ADD MEMBER ICONS
+    team: [],
+    // TODO: ADD IMAGES
+    image: "",
+  },
+];
