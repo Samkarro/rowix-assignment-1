1101ff6cf0394562236450e9379135abba85c006
Luka Gogichaishvili <luka595664@gmail.com>
2025-12-02 15:58:36 +0400

    feat: header styles

--- nexgen/src/common/elements/header.tsx
+++ nexgen/src/common/elements/header.tsx
@@ -1,15 +1,21 @@
 import { headerButtons } from "../data/header-buttons";
+import "../styles/header.styles.css";
 
 export default function NexGenHeader() {
   return (
-    <header>
+    <header className="gray-box">
       <div className="header-logo-container">
         <img alt="NexGen Logo." src="./images/nexgen.png" />
       </div>
       <div className="header-buttons-container">
-        {headerButtons.map((text) => {
+        {headerButtons.map((text, index) => {
           return (
-            <div key={text} className="header-button roboto-mono">
+            <div
+              key={text}
+              className={`header-button roboto-mono clickable ${
+                index === headerButtons.length - 1 ? "cta" : ""
+              } ${index === 0 ? "header-active" : ""}`}
+            >
               {text.toUpperCase()}
             </div>
           );
