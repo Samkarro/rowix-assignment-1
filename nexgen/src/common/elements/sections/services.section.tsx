9ea2bd47f8143407b5569c61450a8f88f7b39dad
Luka Gogichaishvili <luka595664@gmail.com>
2025-12-03 12:13:11 +0400

    feat: styled service boxes

--- nexgen/src/common/elements/sections/services.section.tsx
+++ nexgen/src/common/elements/sections/services.section.tsx
@@ -3,14 +3,14 @@ import "../../styles/services.styles.css";
 
 export default function Services() {
   return (
-    <section id="services">
-      <div className="services-heading-container">
+    <section id="services" className="outlined-box">
+      <div className="services-heading-container gray-box">
         <h2>OUR SERVICES</h2>
       </div>
       <div className="services-container">
         {services.map((service) => {
           return (
-            <div className="service">
+            <div className="service gray-box">
               <div className="upper-area">
                 <div className="upper-service-left">
                   <div className="service-image-container">
@@ -33,8 +33,10 @@ export default function Services() {
                   </p>
                 </div>
               </div>
-              <p>{service.description}</p>
-              <p className="service-price">{service.startingPrice}</p>
+              <div className="lower-service">
+                <p>{service.description}</p>
+                <p className="service-price">{service.startingPrice}</p>
+              </div>
             </div>
           );
         })}
