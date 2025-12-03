d442cfb1af1517d36eff05caf9497aa6ff53b277
Luka Gogichaishvili <luka595664@gmail.com>
2025-12-03 12:06:37 +0400

    feat: separated services section

---
+++ nexgen/src/common/elements/sections/services.section.tsx
@@ -0,0 +1,44 @@
+import { services } from "@/common/data/services";
+import "../../styles/services.styles.css";
+
+export default function Services() {
+  return (
+    <section id="services">
+      <div className="services-heading-container">
+        <h2>OUR SERVICES</h2>
+      </div>
+      <div className="services-container">
+        {services.map((service) => {
+          return (
+            <div className="service">
+              <div className="upper-area">
+                <div className="upper-service-left">
+                  <div className="service-image-container">
+                    <img src={service.image} />
+                  </div>
+                  <h3>{service.heading}</h3>
+                </div>
+                <div className="upper-service-right">
+                  <div className="open-button-container">
+                    <img
+                      alt="Open button."
+                      src="./images/top-right-arrow.png"
+                    />
+                  </div>
+                  <p
+                    className="achievement-heading roboto-mono"
+                    style={{ fontSize: "20px" }}
+                  >
+                    BOOK A CALL
+                  </p>
+                </div>
+              </div>
+              <p>{service.description}</p>
+              <p className="service-price">{service.startingPrice}</p>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
