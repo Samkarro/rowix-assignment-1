8ffca2ab1e9e9bb0e5a3a3f7ebd4f09e1e3d2a7f
Luka Gogichaishvili <luka595664@gmail.com>
2025-12-03 13:53:59 +0400

    feat: separate projects from main page

---
+++ nexgen/src/common/elements/sections/projects.section.tsx
@@ -0,0 +1,90 @@
+import { projects } from "@/common/data/projects";
+import "../../styles/projects.styles.css";
+
+export default function Projects() {
+  return (
+    <section id="projects">
+      <div className="projects-heading-container">
+        <h2>OUR WORKS</h2>
+        <div>
+          <div className="open-button-container">
+            <img alt="Open button." src="./images/top-right-arrow.png" />
+          </div>
+          <p
+            className="achievement-heading roboto-mono"
+            style={{ fontSize: "20px" }}
+          >
+            BOOK A CALL
+          </p>
+        </div>
+      </div>
+      {projects.map((project) => {
+        return (
+          <div className="project">
+            <div className="project-left">
+              <div className="project-top">
+                <div className="service-image-container">
+                  <img src={project.image} />
+                </div>
+                <h3>{project.title}</h3>
+                <div className="upper-service-right">
+                  <div className="open-button-container">
+                    <img
+                      alt="Open button."
+                      src="./images/top-right-arrow.png"
+                    />
+                  </div>
+                  <p
+                    className="achievement-heading roboto-mono"
+                    style={{ fontSize: "20px" }}
+                  >
+                    DETAILS
+                  </p>
+                </div>
+              </div>
+              <div className="tags-container">
+                <div className="project-tag">
+                  <p className="tag-name">Category</p>
+                  <p className="tag-title">{project.category}</p>
+                </div>
+                <div className="project-tag">
+                  <p className="tag-name">Time Taken</p>
+                  <p className="tag-title">{project.timeTaken}</p>
+                </div>
+              </div>
+              <p className="project-description">{project.description}</p>
+            </div>
+            <div className="project-center">
+              <img src={project.image} />
+            </div>
+            <div className="project-right">
+              <div className="tech-container">
+                <h4>TECHNOLOGIES USED</h4>
+                <div className="tech-tags">
+                  {project.tech.map((techItem) => {
+                    return <p>{techItem}</p>;
+                  })}
+                </div>
+              </div>
+              <div className="team-member-container">
+                <h4>TEAM MEMBERS</h4>
+                <div className="team-box">
+                  {project.team.map((member) => {
+                    return (
+                      <div className="team-member">
+                        <img src={member} />
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="CTA">
+                <p>BOOK A CALL</p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </section>
+  );
+}
